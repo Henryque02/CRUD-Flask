9564b40d0ad9c4d9ba0e15fb0811b7b4ee3a8ffa
Henryque <Henryque.Affiune10@gmail.com>
2026-03-09 23:27:39 -0300

    Adiciona estrutura inicial do flask com o metodo create já implementado e atualiza as dependencias

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+
+# Se executado de forma manual __name__ = "__main__"
+app = Flask(__name__)
+
+tasks = []
+task_id_control = 1
+
+@app.route("/tasks", methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"menssage": "Nova tarefa criada com sucesso!"})
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
+
+
+"""
+@app.route("/")
+def hello_world():
+    return "Hello world!"
+
+@app.route("/about")
+def about():
+    return "Página sobre"
+"""
